7a09eea3e379aeeb82853210c19270bc7300ad4c
Ankit <ankitrajpsr@gmail.com>
2023-08-01 11:19:14 +0530

    Changed About

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function myFunction() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
 /*============= toggle icon navbar =============*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
